660b970d3482642b11f72363220d7edeb95786d2
Isherf <isherftheg@gmail.com>
2025-09-14 01:25:01 +0630

    Updated footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,13 +9,13 @@ function Footer() {
       <div className="footer-content">
         <p>
           &copy; {currentYear} &nbsp;
-          <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/IsherfG" target="_blank" rel="noopener noreferrer">
             Isherf
           </a>
           &nbsp; // Built with React & Firebase
         </p>
         <p>
-          <a href="https://github.com/your-username/fanquiz-maker" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/IsherfG/fun-quiz" target="_blank" rel="noopener noreferrer">
             View Source Code on GitHub
           </a>
         </p>
